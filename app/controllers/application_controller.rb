b763c3fed12d1681153c29d64ceef60fa11da5f4
Dmitri Mendis <2dmendis@gmail.com>
2018-07-09 00:58:50 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,15 +11,21 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params[:username])
+    if @user == nil 
+      erb :error
+    end 
+    session[:user_id] = @user.id 
+    redirect '/account'
   end
 
   get '/account' do
-
+    erb :account
   end
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
 
 
